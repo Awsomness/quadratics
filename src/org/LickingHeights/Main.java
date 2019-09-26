bdc9540047506b7eb596790442eb3cb4aeb23512
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-09-26 09:53:45 -0400

    added todo steps

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,37 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFaction,x,y
+
+        //todo initialize manually
+
+        //todo calculations
+
+        //todo print results
+
+        //todo not in code, check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
